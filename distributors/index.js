048367c89fae090e79fc5fd260232fc8bc884b9f
Benjamin Pannell <admin@sierrasoftworks.com>
2015-01-23 21:59:34 +0200

    Improved both EndpointProvider and DistributorProvider

--- distributors/index.js
+++ distributors/index.js
@@ -1,22 +1,33 @@
 var fs = require('fs'),
     path = require('path'),
-    _ = require('lodash');
+    _ = require('lodash'),
+    log = require('debug')('kong:distributors:log'),
+    debug = require('debug')('kong:distributors:debug');
 
 function DistributionProvider(server) {
+  Object.defineProperty(this, 'server', {
+    value: server,
+    configurable: false,
+    enumerable: false
+  });
 }
 
 DistributionProvider.prototype.register = function(name, distributor) {
-  if(~Object.keys(DistributionProvider.prototype).indexOf(name) || Object.keys(this).indexOf(name)) throw new Error("Cannot register a distributor with the name " + name + " as this name is reserved");
-  this[name] = distributor.bind(null, server);
+  if(~Object.keys(DistributionProvider.prototype).indexOf(name) || ~Object.keys(this).indexOf(name)) return this.server.error(new Error("Cannot register a distributor with the name " + name + " as this name is reserved"));
+  debug('registered %s', name);
+  this[name] = distributor.bind(null, this.server);
 };
 
 DistributionProvider.prototype.registerAll = function() {
   var directory = path.resolve(__dirname, "providers");
 
   _.each(fs.readdirSync(directory), function(filename) {
-    if(/\.js$/.test(filename)) this.register(
-      filename.replace(/(.+)\.js$/, function(_, name) { return name; }),
-      require(path.resolve(directory, filename)));
+    if(/\.js$/.test(filename)) {
+      log("registering %s", filename.substr(0, filename.length - 3));
+      this.register(
+        filename.substr(0, filename.length - 3),
+        require(path.resolve(directory, filename)));
+    }
     }, this);
   };
 
--- endpoints/index.js
+++ endpoints/index.js
@@ -1,23 +1,32 @@
 var fs = require('fs'),
-  path = require('path'),
-  _ = require('lodash');
+    path = require('path'),
+    _ = require('lodash'),
+    log = require('debug')('kong:endpoints:log'),
+    debug = require('debug')('kong:endpoints:debug');
 
 function EndpointProvider(server) {
-  this.server = server;
+  Object.defineProperty(this, 'server', {
+    value: server,
+    configurable: false,
+    enumerable: false
+  });
 }
 
 EndpointProvider.prototype.register = function(name, endpoint) {
-  if(~Object.keys(EndpointProvider.prototype).indexOf(name) || Object.keys(this).indexOf(name)) throw new Error("Cannot register an endpoint with the name " + name + " as this name is reserved");
-  this[name] = endpoint(server, server.notify.bind(server, name));
+  if(~Object.keys(EndpointProvider.prototype).indexOf(name) || ~Object.keys(this).indexOf(name)) return this.server.error(new Error("Cannot register an endpoint with the name " + name + " as this name is reserved"));
+  this[name] = endpoint(this.server, this.server.notify.bind(this.server, name));
 };
 
 EndpointProvider.prototype.registerAll = function() {
   var directory = path.resolve(__dirname, "providers");
 
   _.each(fs.readdirSync(directory), function(filename) {
-    if(/\.js$/.test(filename)) this.register(
-      filename.replace(/(.+)\.js$/, function(_, name) { return name; }),
-      require(path.resolve(directory, filename)));
+    if(/\.js$/.test(filename)) {
+      log("registering %s", filename.substr(0, filename.length - 3));
+      this.register(
+        filename.substr(0, filename.length - 3),
+        require(path.resolve(directory, filename)));
+    }
   }, this);
 };
 
