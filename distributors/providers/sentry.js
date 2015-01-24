e902ba2c6dbe149ee204dadaf5170620e3165ba4
Benjamin Pannell <admin@sierrasoftworks.com>
2015-01-24 14:42:58 +0200

    Added a Sentry distributor

---
+++ distributors/providers/sentry.js
@@ -0,0 +1,20 @@
+var _ = require('lodash'),
+    Q = require('q');
+
+module.exports = function(server, notification) {
+  var defered = Q.defer();
+
+  _.defaults(notification, {
+    level: 'log',
+    version: server.config.version
+  });
+
+  var message = notification.message;
+  delete notification.message;
+
+  server.raven.captureMessage(message, notification, function() {
+    defered.resolve();
+  });
+
+  return defered.promise;
+};
